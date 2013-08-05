43086f6cd0434fa6d1a88f199dbdcd2c9d8832f3
Andrew Hebb <hebb.andrew@gmail.com>
2013-08-05 17:44:32 -0300

    Two hands. Flush and straight identification.

---
+++ README.md
@@ -0,0 +1,4 @@
+Poker: Texas hold 'em
+===================
+
+This program will deal two two-card hands and five community cards. It will then say whether each player has a flush or a straight.
--- poker.cpp
+++ poker.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,43 +8,144 @@ struct card
 {
 	string rank;
 	string suit;
-};
-struct hand
-{
-	card first;
-	card second;
+	int rank_number;
+	int suit_number;
 };
 
 void draw_card(bool *deck, card *card);
 void shuffle_deck(bool *deck);
+int evaluate(vector<card> hand);
+bool isFlush(vector<card> hand);
+bool isStraight(vector<card> hand);
 
 int main()
 {
 	bool deck[52];
 	shuffle_deck(deck);
-	hand player1;
-	draw_card(deck, &player1.first);
-	draw_card(deck, &player1.second);
+	/*
+	card player1[2];
+	card player2[2];
+	card board[5];
+	card hand1[7];
+	card hand2[7];
+	*/
+	
+	vector<card> player1 (2);
+	vector<card> player2 (2);
+	vector<card> board (5);
+	vector<card> hand1 (7);
+	vector<card> hand2 (7);
+	
+	int i;
+	
+	
+	draw_card(deck, &player1[0]);
+	draw_card(deck, &player2[0]);
+	draw_card(deck, &player1[1]);
+	draw_card(deck, &player2[1]);
+	draw_card(deck, &board[0]);
+	draw_card(deck, &board[1]);
+	draw_card(deck, &board[2]);
+	draw_card(deck, &board[3]);
+	draw_card(deck, &board[4]);
+	
+	
+	// test
+	/*
+	player1[0].rank_number = 3;
+	player1[0].rank = "Three";
+	player1[0].suit_number = 1;
+	player1[0].suit = "Spades";
+	player1[1].rank_number = 9;
+	player1[1].rank = "Nine";
+	player1[1].suit_number = 4;
+	player1[1].suit = "Clubs";
+	player2[0].rank_number = 8;
+	player2[0].rank = "Eight";
+	player2[0].suit_number = 4;
+	player2[0].suit = "Clubs";
+	player2[1].rank_number = 13;
+	player2[1].rank = "King";
+	player2[1].suit_number = 2;
+	player2[1].suit = "Hearts";
+	board[0].rank_number = 11;
+	board[0].rank = "Jack";
+	board[0].suit_number = 2;
+	board[0].suit = "Hearts";
+	board[1].rank_number = 2;
+	board[1].rank = "Two";
+	board[1].suit_number = 3;
+	board[1].suit = "Diamonds";
+	board[2].rank_number = 12;
+	board[2].rank = "Queen";
+	board[2].suit_number = 1;
+	board[2].suit = "Spades";
+	board[3].rank_number = 10;
+	board[3].rank = "Ten";
+	board[3].suit_number = 3;
+	board[3].suit = "Diamonds";
+	board[4].rank_number = 8;
+	board[4].rank = "Eight";
+	board[4].suit_number = 3;
+	board[4].suit = "Diamonds";
+	*/
+	
+	for (i=0;i<2;i++)
+	{
+		hand1[i] = player1[i];
+		hand2[i] = player2[i];
+	}
+	for (i=0;i<5;i++)
+	{
+		hand1[i+2] = board[i];
+		hand2[i+2] = board[i];
+	}
 
-	cout << "Player 1 was dealt ";
-	if (player1.first.rank == "Ace" || player1.first.rank == "Eight")
+	cout << "Player 1 was dealt the " << player1[0].rank << " of " << player1[0].suit << " and the " << player1[1].rank << " of " << player1[1].suit << "." << endl;
+	cout << "Player 2 was dealt the " << player2[0].rank << " of " << player2[0].suit << " and the " << player2[1].rank << " of " << player2[1].suit << "." << endl;
+
+	cout << "The community cards are " << endl;
+	cout << "the " << board[0].rank << " of " << board[0].suit << "," << endl;
+	cout << "the " << board[1].rank << " of " << board[1].suit << "," << endl;
+	cout << "the " << board[2].rank << " of " << board[2].suit << "," << endl;
+	cout << "the " << board[3].rank << " of " << board[3].suit << "," << endl;
+	cout << "and the " << board[4].rank << " of " << board[4].suit << "." << endl;
+
+	if (isFlush(hand1))
 	{
-		cout << "an ";
+		cout << "Player 1 has a flush." << endl;
 	}
 	else
 	{
-		cout << "a ";
+		cout << "Player 1 does not have a flush." << endl;
 	}
-	cout << player1.first.rank << " of " << player1.first.suit << " and ";
-	if (player1.second.rank == "Ace" || player1.second.rank == "Eight")
+	
+	if (isFlush(hand2))
 	{
-		cout << "an ";
+		cout << "Player 2 has a flush." << endl;
 	}
 	else
 	{
-		cout << "a ";
+		cout << "Player 2 does not have a flush." << endl;
 	}
-	cout << player1.second.rank << " of " << player1.second.suit << "." << endl;
+	
+	if (isStraight(hand1))
+	{
+		cout << "Player 1 has a straight." << endl;
+	}
+	else
+	{
+		cout << "Player 1 does not have a straight." << endl;
+	}
+	
+	if (isStraight(hand2))
+	{
+		cout << "Player 2 has a straight." << endl;
+	}
+	else
+	{
+		cout << "Player 2 does not have a straight." << endl;
+	}
 
 	return 0;
 }
@@ -59,6 +161,8 @@ void draw_card(bool *deck, card *card)
 	deck[n] = 1;
 	rank = n % 13 + 1;
 	suit = n / 13 + 1;
+	card->rank_number = rank;
+	card->suit_number = suit;
 	switch (rank)
 	{
 		case 1: card->rank = "Ace"; break;
@@ -91,4 +195,80 @@ void shuffle_deck(bool *deck)
 	{
 		deck[i] = 0;
 	}
+}
+
+int evaluate(vector<card> hand)
+{
+	return 0;
+}
+
+bool isFlush(vector<card> hand)
+{
+	int suits[4] = {0, 0, 0, 0};
+	int i, limit;
+	
+	limit = hand.size();
+	
+	for (i=0;i<limit;i++)
+	{
+		suits[hand[i].suit_number-1] += 1;
+	}
+	for (i=0;i<4;i++)
+	{
+		if (suits[i]>4)
+		{
+			return 1;
+		}
+	}
+	
+	return 0;
+}
+
+bool isStraight(vector<card> hand)
+{
+	int i, j, head, tail, limit;
+	
+	limit = hand.size();
+	
+	int rank[limit];
+	
+	for (i=0; i<limit; i++)
+	{
+		rank[i] = hand[i].rank_number;
+	}
+	
+	for(j=0; j < (limit - 4); j++)
+	{
+		head = rank[j];
+		tail = rank[j];
+		
+		for (i=j+1; i<limit; i++)
+		{
+			
+			if (rank[i] == head - 1)
+			{
+				head = rank[i];
+				i = j;
+			}
+			else if (rank[i] == tail + 1)
+			{
+				tail = rank[i];
+				i = j;
+			}
+			if (tail - head == 4)
+			{
+				return 1;
+			}
+		}
+		
+		for (i=limit-5; i<4; i++)
+		{
+			if (tail - head == i)
+			{
+				return 0;
+			}
+		}
+	}
+	
+	return 0;
 }
